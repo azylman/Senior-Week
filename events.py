c5a3d9a4ec1403a2a5ff929fc9f7002bb4e3777f
Alex Zylman <alex@zylman.com>
2012-05-15 11:45:38 -0500

    add handler for specific event days

--- events.py
+++ events.py
@@ -9,6 +9,12 @@ class MainPage(webapp2.RequestHandler):
   def get(self):
     template = jinja_environment.get_template('templates/events/events.html')
     self.response.out.write(template.render())
+	
+class EventDay(webapp2.RequestHandler):
+  def get(self, date):
+    template = jinja_environment.get_template('templates/events/events-' + date + '.html')
+    self.response.out.write(template.render())
 
-app = webapp2.WSGIApplication([('/events', MainPage)],
+app = webapp2.WSGIApplication([('/events', MainPage),
+							   (r'/events/(.*)', EventDay)],
                               debug=True)
